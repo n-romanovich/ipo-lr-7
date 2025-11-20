d245104e38eac7edbfb49ffc4c1cc0d91bc4390d
Romanovicn Nikita <romanovichikita1@gmail.com>
2025-11-20 12:22:23 +0300

    Using functions

--- task_2/main.py
+++ task_2/main.py
@@ -1,25 +1,40 @@
 import json #Модуль json
 
-f = open("dump.json", "r", encoding="utf-8") #Открытие файла
-data = json.load(f) #Загрузка данных
-f.close() #Закрытие файла
+found = False
+
+#Функция вывода данных на экран
+def disp_data(code):
+    global found    #Использование глобальной переменной found
+
+    for item in data: #Перебор данных
+        if item.get("model") == "data.specialty": #Проверка на специальность
+            fields = item.get("fields", {}) #Получение полей
+            if fields.get("code") == code: #Сравнение кода
+                print("============== Найдено ===============") #Вывод заголовка
+                print(fields.get("code"), ">> Специальность \"" + fields.get("title") + "\", " + fields.get("c_type")) #Вывод специальности
+                found = True #Меняем флаг
+                for skill in data: #Цикл по всем объектам для поиска квалификаций
+                    if skill.get("model") == "data.skill": #Проверка на квалификацию
+                        s_fields = skill.get("fields", {}) #Получение полей
+                        if s_fields.get("code", "").startswith(code): #Проверка кода
+                            print(s_fields.get("code"), ">> Квалификация \"" + s_fields.get("title") + "\"") #Вывод квалификации
+
+    if not found: #Если ничего не найдено
+        print("============== Не найдено ===============") #Вывод сообщения
+
+#Функция открытия файла
+def file_open():
+    f = open("dump.json", "r", encoding="utf-8") #Открытие файла
+    data = json.load(f) #Загрузка данных
+    f.close() #Закрытие файла
+
+    return data
+
+
+
+data = file_open() #Вызов функции открытия файла
 
 code = input("Введите номер квалификации: ") #Ввод кода квалификации
 
-found = False #Флаг результата поиска
+disp_data(code) #Вызов функциии вывода данных на экран
 
-for item in data: #Перебор данных
-    if item.get("model") == "data.specialty": #Проверка на специальность
-        fields = item.get("fields", {}) #Получение полей
-        if fields.get("code") == code: #Сравнение кода
-            print("============== Найдено ===============") #Вывод заголовка
-            print(fields.get("code"), ">> Специальность \"" + fields.get("title") + "\", " + fields.get("c_type")) #Вывод специальности
-            found = True #Меняем флаг
-            for skill in data: #Цикл по всем объектам для поиска квалификаций
-                if skill.get("model") == "data.skill": #Проверка на квалификацию
-                    s_fields = skill.get("fields", {}) #Получение полей
-                    if s_fields.get("code", "").startswith(code): #Проверка кода
-                        print(s_fields.get("code"), ">> Квалификация \"" + s_fields.get("title") + "\"") #Вывод квалификации
-
-if not found: #Если ничего не найдено
-    print("============== Не найдено ===============") #Вывод сообщения
--- task_3/main.py
+++ task_3/main.py
@@ -1,8 +1,7 @@
 import json #Импорт json
 
-operations = 0 #Счетчик операций
-
-while True: #Бесконечный цикл
+#Функция вывода меню
+def disp_menu():
     print("\nМеню:") #Вывод меню
     print("1 - Вывести все записи") #Пункт 1
     print("2 - Вывести запись по полю (id)") #Пункт 2
@@ -10,8 +9,8 @@ while True: #Бесконечный цикл
     print("4 - Удалить запись по полю (id)") #Пункт 4
     print("5 - Выйти из программы") #Пункт 5
 
-    choice = input("Ваш выбор: ") #Ввод пункта меню
-
+#Функция открытия файла
+def file_open():
     f = open("stars.json", "r", encoding="utf-8") #Открыть файл
     content = f.read().strip() #Прочитать содержимое
     f.close() #Закрыть файл
@@ -19,68 +18,95 @@ while True: #Бесконечный цикл
         data = [] #Список пустой
     else:
         data = json.loads(content) #Загрузить данные
+        return data
 
-    if choice == "1": #Вывод всех записей
-        for i in range(len(data)): #Цикл по списку
-            print("---------------------- Запись", i+1, "-----------------------") #Заголовок
-            print("id:", data[i]["id"]) #Вывод id
-            print("Название:", data[i]["name"]) #Вывод названия
-            print("Созвездие:", data[i]["constellation"]) #Вывод созвездия
-            print("Видна без телескопа:", data[i]["is_visible"]) #Вывод видимости
-            print("Радиус (в солнечных):", data[i]["radius"]) #Вывод радиуса
-        operations += 1 #Прибавить операцию
+#Функция выбора из меню номер 1
+def menu_one(): 
+    global operations   #Использовать глобальную переменную operations
 
-    elif choice == "2": #Вывод записи по id
-        search_id = int(input("Введите id: ")) #Ввод id
-        found = False #Флаг поиска
-        for i in range(len(data)): #Цикл
-            if data[i]["id"] == search_id: #Сравнение id
-                print("Позиция в списке:", i) #Вывод позиции
-                print("Запись:", data[i]) #Вывод записи
-                found = True #Нашли
-        if not found: #Если не нашли
+    for i in range(len(data)): #Цикл по списку
+        print("---------------------- Запись", i+1, "-----------------------") #Заголовок
+        print("id:", data[i]["id"]) #Вывод id
+        print("Название:", data[i]["name"]) #Вывод названия
+        print("Созвездие:", data[i]["constellation"]) #Вывод созвездия
+        print("Видна без телескопа:", data[i]["is_visible"]) #Вывод видимости
+        print("Радиус (в солнечных):", data[i]["radius"]) #Вывод радиуса
+    operations += 1 #Прибавить операцию
+
+#Функция выбора из меню номер 2
+def menu_two():
+    global operations   #Использовать глобальную переменную operations
+    
+    search_id = int(input("Введите id: ")) #Ввод id
+    found = False #Флаг поиска
+    for i in range(len(data)): #Цикл
+        if data[i]["id"] == search_id: #Сравнение id
+            print("Позиция в списке:", i) #Вывод позиции
+            print("Запись:", data[i]) #Вывод записи
+            found = True #Нашли
+        if found == False: #Если не нашли
             print("Запись с таким id не найдена") #Сообщение
-        operations += 1 #Прибавить операцию
+    operations += 1 #Прибавить операцию
 
-    elif choice == "3": #Добавить запись
-        new_id = int(input("Введите id: ")) #Ввод id
-        new_name = input("Введите название: ") #Ввод названия
-        new_constellation = input("Введите созвездие: ") #Ввод созвездия
-        new_visible = input("Видна без телескопа (True/False): ") #Ввод видимости
-        new_radius = float(input("Введите радиус (в солнечных): ")) #Ввод радиуса
-        data.append({ #Добавить словарь
-            "id": new_id,
-            "name": new_name,
-            "constellation": new_constellation,
-            "is_visible": new_visible == "True",
-            "radius": new_radius
-        })
-        f = open("stars.json", "w", encoding="utf-8") #Открыть файл на запись
+#Функция выбора из меню номер 3
+def menu_three():
+    global operations   #Использовать глобальную переменную operations
+
+    new_id = int(input("Введите id: ")) #Ввод id
+    new_name = input("Введите название: ") #Ввод названия
+    new_constellation = input("Введите созвездие: ") #Ввод созвездия
+    new_visible = input("Видна без телескопа (True/False): ") #Ввод видимости
+    new_radius = float(input("Введите радиус (в солнечных): ")) #Ввод радиуса
+    data.append({ #Добавить словарь
+        "id": new_id,
+        "name": new_name,
+        "constellation": new_constellation,
+        "is_visible": new_visible == "True",
+        "radius": new_radius
+    })
+    f = open("stars.json", "w", encoding="utf-8") #Открыть файл на запись
+    json.dump(data, f, ensure_ascii=False, indent=2) #Сохранить список
+    f.close() #Закрыть файл
+    print("Запись добавлена") #Сообщение
+    operations += 1 #Прибавить операцию
+
+#Функция выбора из меню номер 4
+def menu_four():
+    global operations   #Использовать глобальную переменную operations
+
+    del_id = int(input("Введите id для удаления: ")) #Ввод id
+    found = False #Флаг
+    for i in range(len(data)): #Цикл
+        if data[i]["id"] == del_id: #Сравнение id
+            del data[i] #Удалить
+            found = True #Нашли
+            break #Прервать цикл
+    if found: #Если нашли
+        f = open("stars.json", "w", encoding="utf-8") #Открыть файл
         json.dump(data, f, ensure_ascii=False, indent=2) #Сохранить список
         f.close() #Закрыть файл
-        print("Запись добавлена") #Сообщение
-        operations += 1 #Прибавить операцию
+        print("Запись удалена") #Сообщение
+    else: #Если не нашли
+        print("Запись с таким id не найдена") #Сообщение
+    operations += 1 #Прибавить операцию
 
-    elif choice == "4": #Удалить запись
-        del_id = int(input("Введите id для удаления: ")) #Ввод id
-        found = False #Флаг
-        for i in range(len(data)): #Цикл
-            if data[i]["id"] == del_id: #Сравнение id
-                del data[i] #Удалить
-                found = True #Нашли
-                break #Прервать цикл
-        if found: #Если нашли
-            f = open("stars.json", "w", encoding="utf-8") #Открыть файл
-            json.dump(data, f, ensure_ascii=False, indent=2) #Сохранить список
-            f.close() #Закрыть файл
-            print("Запись удалена") #Сообщение
-        else: #Если не нашли
-            print("Запись с таким id не найдена") #Сообщение
-        operations += 1 #Прибавить операцию
+#Функция выбора из меню номер 5
+def menu_five():
+    global operations   #Использовать глобальную переменную operations
 
-    elif choice == "5": #Выход
-        print("Количество выполненных операций:", operations) #Вывод счетчика
-        break #Прервать цикл
+    print("Количество выполненных операций:", operations) #Вывод счетчика
+    quit()  #Выход из программы
 
-    else: #Иначе
-        print("Неверный пункт меню") #Сообщение
+
+
+operations = 0 #Счетчик операций
+functions = [menu_one, menu_two, menu_three, menu_four, menu_five]  #Список с функциями
+data = file_open()
+
+while True: #Бесконечный цикл
+
+    disp_menu() #Вызов функции с выводом на экран меню
+
+    choice = input("Ваш выбор: ") #Ввод пункта меню
+
+    functions[int(choice) - 1]()  #Выбор функции по ее номеру из списка functions
