ea5cba97e57f9a3ae34953f3f557d9661118fb67
Romanovicn Nikita <romanovichikita1@gmail.com>
2025-12-03 09:28:07 +0300

    Task 2 complete

---
+++ ipo-lr-10/collision/collision.py
@@ -0,0 +1,6 @@
+def isCorrectRect(list):
+    if list[0][0] < list[1][0] and list[0][1] < list[1][1]:
+        return True
+    else:
+        return False
+
---
+++ ipo-lr-10/main.py
@@ -0,0 +1,5 @@
+from collision.collision import isCorrectRect
+
+print(isCorrectRect([(-3.4, 1),(9.2, 10)]))
+
+print(isCorrectRect([(-7, 9),(3, 6)]))
