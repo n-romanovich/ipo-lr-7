6728f1fb0c20197d2f8ffe74ca3d1ecf2ddd9441
Romanovicn Nikita <romanovichikita1@gmail.com>
2025-12-03 09:31:21 +0300

    Task 2 complete

--- ipo-lr-10/collision/collision.py
+++ ipo-lr-10/collision/collision.py
@@ -1,3 +1,4 @@
+#Функция для проверки прямоугольника на правильность
 def isCorrectRect(list):
     if list[0][0] < list[1][0] and list[0][1] < list[1][1]:
         return True
--- ipo-lr-10/main.py
+++ ipo-lr-10/main.py
@@ -1,5 +1,5 @@
 from collision.collision import isCorrectRect
 
-print(isCorrectRect([(-3.4, 1),(9.2, 10)]))
+print(isCorrectRect([(-3.4, 1),(9.2, 10)])) #Вызов функции проверки прямоугольника, который возвращает True
 
-print(isCorrectRect([(-7, 9),(3, 6)]))
+print(isCorrectRect([(-7, 9),(3, 6)]))  #Вызов функции проверки прямоугольника, который возвращает False
